894fb10f7599ead58965fad6f79d9883ba53c1b6
Karolina Siwkowska <trallala00@gmail.com>
2018-04-29 22:05:46 +0200

    Init project

---
+++ components/App.js
@@ -0,0 +1,82 @@
+var GIPHY_API_URL = 'https://api.giphy.com',
+    GIPHY_PUB_KEY = '9af6d92a7b0d48a4812c800105edda24';
+
+
+App = React.createClass({
+
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+
+    getGif: function (searchingText, callback) {
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText).data;
+                var gif = {
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);
+            }
+        };
+        xhr.send();
+    },
+
+
+    handleSearch: function (searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText, function (gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
+
+
+
+    render: function () {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return ( <
+            div style = {
+                styles
+            } >
+            <
+            h1 > Wyszukiwarka GIFow! < /h1> <
+            p > Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
+            <
+            Search onSearch = {
+                this.handleSearch
+            }
+            /> <
+            Gif loading = {
+                this.state.loading
+            }
+            url = {
+                this.state.gif.url
+            }
+            sourceUrl = {
+                this.state.gif.sourceUrl
+            }
+            /> <
+            /div>
+        );
+    }
+});
